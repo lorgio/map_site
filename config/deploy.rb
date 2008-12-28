7002619a682f0003dc06647cee3b57b67a675715
lorgio <lorgio@lorgio-jimenezs-macbook-pro.local>
2008-12-28 18:15:31 -0500

    modified deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,38 +18,55 @@
 #	Application
 #############################################################
 
-set :application, "blog"
-set :deploy_to, "/home/lorgioj/www/#{application}"
+set :application, "map_site"
+# set :deploy_to, "/home/lorgioj/www/#{application}"
+set :deploy_to, "/home/lorgioj/#{application}"
 
 #############################################################
 #	Settings
 #############################################################
 
-default_run_options[:pty] = true
-set :use_sudo, true
+# default_run_options[:pty] = true
+set :use_sudo, false
 
+ssh_options[:compression] = false
 #############################################################
 #	Servers
 #############################################################
 
 set :user, "lorgioj"
 set :domain, "lorgiojimenez.com"
-server domain, :app, :web
-role :db, domain, :primary => true
+role :app, "lorgiojimenez.com"
+role :web, "lorgiojimenez.com"
+role :db,  "lorgiojimenez.com", :primary => true
+# server domain, :app, :web
+# role :db, domain, :primary => true
 
 #############################################################
-#	Subversion
+#	Git
 #############################################################
+set :scm, "git"
+set :repository,  "git@github.com:lorgio/map_site.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+default_run_options[:pty] = true
 
-set :repository,  "http://www.example.com/svn/example"
-set :svn_username, "jim"
-set :svn_password, "password"
-set :checkout, "export"
+# set :scm_passphrase, "asdflkj1234" #This is your custom users password
+# set :user, "deployer"
 
 #############################################################
 #	Passenger
 #############################################################
 
+# namespace :deploy do
+#   desc "Restarting mod_rails with restart.txt"
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "touch #{current_path}/tmp/restart.txt"
+#   end
+
+deploy.task :start do
+  # nothing happens so that the script/spin nohup crap doesn't fail
+end
 namespace :passenger do
   desc "Restart Application"
   task :restart do
